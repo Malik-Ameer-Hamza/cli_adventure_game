6e624f8be28fc4b777a76f5760f90580f38481c9
Malik-Ameer-Hamza <ameerhamza205242@gmail.com>
2024-02-11 13:26:34 +0500

    Initiazlied the repo

---
+++ game.js
@@ -0,0 +1,105 @@
+import inquirer from "inquirer";
+import { createSpinner } from "nanospinner";
+import chalk from "chalk";
+import showBanner from "node-banner";
+let sleep = (time = 2000) => new Promise((r) => setTimeout(r, time));
+let banner = async () => {
+    await showBanner("Adventure Game");
+};
+await banner();
+await sleep(1000);
+class Player {
+    name;
+    health = 100;
+    constructor(name) {
+        this.name = name;
+    }
+}
+class Opponent {
+    name;
+    health = 100;
+    constructor(name) {
+        this.name = name;
+    }
+}
+let { player } = await inquirer.prompt([{
+        name: "player",
+        type: "input",
+        message: "Enter your name: "
+    }]);
+let { opponent } = await inquirer.prompt([{
+        name: "opponent",
+        type: "list",
+        message: "Select your opponent: ",
+        choices: [`Skeleton`, `Warrior`, `Zombie`, `Assassin`]
+    }]);
+let play = new Player(player);
+let opp = new Opponent(opponent);
+console.log(chalk.bold(`Instructions:`));
+console.log(`${chalk.redBright(`>>>`)} You can damage enemy upto 50 health`);
+console.log(`${chalk.redBright(`>>>`)} Enemy can damage you upto 50 health`);
+let spinner = createSpinner("Loading Game").start();
+await sleep();
+spinner.success({ text: "Game Loaded Sucessfully" });
+let NoOfHealthPotion = 3;
+let healthPotion = 30;
+while (true) {
+    console.log(chalk.blueBright(`\n=========================`));
+    console.log(`${play.name} Health : ${play.health}`);
+    console.log(`${opp.name} Health : ${opp.health}`);
+    console.log(chalk.blueBright(`=========================\n`));
+    console.log(`${chalk.yellowBright(`<<<<<<<<<<<<<<`)} ${chalk.blueBright(`${opponent} Has Appeared`)} ${chalk.yellowBright(`<<<<<<<<<<<<<<`)}`);
+    if ([`Skeleton`, `Warrior`, `Zombie`, `Assassin`].includes(opponent)) {
+        let { choices } = await inquirer.prompt([{
+                name: "choices",
+                type: "list",
+                message: "What would you like to do? ",
+                choices: ["Attack", "Drink Health Potion", "Run"]
+            }]);
+        if (choices === "Attack") {
+            console.log((chalk.bgBlack.whiteBright(`\n ___________${chalk.bgRgb(0, 55, 0).greenBright(`/|${play.name}|*`)}${chalk.bgRgb(55, 0, 0).redBright(`*|${opp.name}|\\`)}___________`)));
+            let playerDamage = Math.floor(Math.random() * 50);
+            let opponentDamage = Math.floor(Math.random() * 50);
+            play.health -= playerDamage;
+            opp.health -= opponentDamage;
+            if (play.health <= 0) {
+                console.log(chalk.bold.redBright(`You Lose!`));
+                console.log(`${chalk.redBright(`>>>`)} You have ${chalk.redBright(0)} health left.`);
+                console.log(`${chalk.redBright(`>>>`)} ${opp.name} have ${chalk.redBright(opp.health)} health left`);
+                break;
+            }
+            else if (opp.health <= 0) {
+                console.log(chalk.bold.greenBright(`You Win!`));
+                console.log(`${chalk.redBright(`>>>`)} You have ${chalk.redBright(play.health)} health left`);
+                console.log(`${chalk.redBright(`>>>`)} ${opp.name} health is ${chalk.redBright(0)}.`);
+                break;
+            }
+        }
+        ;
+        if (choices === "Drink Health Potion") {
+            if (play.health >= 70) {
+                console.log(`${chalk.redBright(`>>>`)} Sorry you can't drink health potion your health is greater than 50.`);
+            }
+            else if (NoOfHealthPotion < 1) {
+                console.log(`${chalk.redBright(`>>>`)} Sorry you don't have more Health Potion.`);
+            }
+            else {
+                play.health += healthPotion;
+                NoOfHealthPotion -= 1;
+                let spinner = createSpinner("Drinking health potion...").start();
+                await sleep();
+                spinner.success();
+                console.log(`${chalk.redBright(`>>>`)} You drink a health potion, healing yourself for ${chalk.redBright(30)}`);
+                console.log(`${chalk.redBright(`>>>`)} You now have ${chalk.redBright(play.health)} health.`);
+                console.log(`${chalk.redBright(`>>>`)} You now have ${chalk.redBright(NoOfHealthPotion)} health potion left`);
+            }
+        }
+        ;
+        if (choices === "Run") {
+            console.log(chalk.blueBright(`==============================================`));
+            console.log(chalk.blueBright(`\t Thanks for playing !!!!!`));
+            console.log(chalk.blueBright(`==============================================`));
+            break;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,51 @@
+# Adventure Game
+A thrilling text-based adventure where you battle an opponent!
+
+
+## Description
+This is a simple yet engaging text-based game. Face a formidable opponent in tactical combat, where each choice holds the key to victory. Manage your health strategically, unleash attacks, and make crucial decisions to emerge triumphant.
+
+# Gameplay
+
+## Mechanics:
+
+- Face a single opponent type, offering consistency and focused gameplay.
+- Choose between attacking or using a health potion (limited uses) each round.
+- Attacks deal a fixed amount of damage (30).
+- Manage your health (initially 100) to avoid defeat.
+- Win by reducing your opponent's health to zero before yours depletes.
+
+
+## Installation
+- Ensure you have Node.js and npm installed.
+- Clone or download the repository.
+- Run 
+```bash 
+npm install 
+```
+to install dependencies.
+
+
+## Run in your Terminal
+```bash
+npx ameer-adventure-game
+```
+
+
+## Playing the Game
+- Run node index.js in your terminal.
+- Enter your desired name.
+- Engage in combat by choosing "Attack" or "Drink Health Potion" each round.
+- Strategize your actions to secure victory!
+
+
+## Endings
+Win: Deplete your opponent's health before yours reaches zero.
+Lose: Your health reaches zero before your opponent's.
+
+
+## Additional Information
+- This is a beginner-friendly game designed for simple yet engaging gameplay.
+- Feedback and contributions are welcome!
+
+
